333278857aee3dcdcc08edb3b581cbc6c42ad1bf
Narendra52977 <68866931+Narendra52977@users.noreply.github.com>
2020-11-10 22:53:23 +0530

    e

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,7 @@ class Timer extends React.Component {
       render: false,
       time: 0,
       x: 0,
-      y: 0,
-      ballPosition: { left: "0px", top: "0px" }
+      y: 0
     };
     this.intervalId = 0;
     this.renderChoice = this.renderChoice.bind(this);
@@ -26,60 +25,24 @@ class Timer extends React.Component {
     document.addEventListener("keydown", (event) => {
       if (this.state.render) {
         if (event.keyCode === 39) {
-          this.setState(
-            {
-              x: this.state.x + 5
-            },
-            () =>
-              this.setState({
-                ballPosition: {
-                  left: this.state.x + "px",
-                  top: this.state.y + "px"
-                }
-              })
-          );
+          this.setState({
+            x: this.state.x + 5
+          });
         }
         if (event.keyCode === 40) {
-          this.setState(
-            {
-              y: this.state.y + 5
-            },
-            () =>
-              this.setState({
-                ballPosition: {
-                  left: this.state.x + "px",
-                  top: this.state.y + "px"
-                }
-              })
-          );
+          this.setState({
+            y: this.state.y + 5
+          });
         }
         if (event.keyCode === 37) {
-          this.setState(
-            {
-              x: this.state.x - 5
-            },
-            () =>
-              this.setState({
-                ballPosition: {
-                  left: this.state.x + "px",
-                  top: this.state.y + "px"
-                }
-              })
-          );
+          this.setState({
+            x: this.state.x - 5
+          });
         }
         if (event.keyCode === 38) {
-          this.setState(
-            {
-              y: this.state.y - 5
-            },
-            () =>
-              this.setState({
-                ballPosition: {
-                  left: this.state.x + "px",
-                  top: this.state.y + "px"
-                }
-              })
-          );
+          this.setState({
+            y: this.state.y - 5
+          });
         }
       }
       if (this.state.x === 250 && this.state.y === 250) {
@@ -96,7 +59,10 @@ class Timer extends React.Component {
     return (
       <>
         <div className="hole"></div>
-        <div className="ball" style={this.state.ballPosition}></div>
+        <div
+          className="ball"
+          style={{ left: this.state.x + "px", top: this.state.y + "px" }}
+        ></div>
         <button
           style={{ position: "absolute", top: "150px", left: "150px" }}
           onClick={this.buttonClick}
